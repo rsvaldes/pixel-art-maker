7d87be961ca4b7629c849b5107443efda48b8883
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-04-18 09:54:39 -0600

    still not flowing but it will happen

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var body = document.getElementsByTagName("body")[0];
-body.style.background = "radial-gradient(#FFFFFF 50%, #00ffcc 80%, #e4b6f7  40%)";
+body.style.backgroundImage = "url(prettycolors.png)";
 var div = document.createElement("div");
 div.style.backgroundColor = "white";
 div.style.display = "block";
 div.id = "bigBoy";
 div.addEventListener('click',function tileClick(event) {
-  event.target.style.backgroundColor = crayon.currentColor;
+
+   event.target.style.backgroundColor = crayon.currentColor;
 });
-// div.addEventListener("mousedown", function flowyBrush (event){
-//   event.target.style.backgroundColor = crayon.currentColor}
+
+
+
 var audio = document.createElement("audio");
 body.appendChild(audio);
 audio.src = "superMario.mp3";
@@ -20,7 +22,7 @@ var palette = document.getElementById("palette");
 
 body.appendChild(div);
 function createTiles () {
-  for (var i = 0; i < 2700; i++) {
+  for (var i = 0; i < 3000; i++) {
     var tile = document.createElement("div");
     tile.classList.add('tile');
     div.appendChild(tile);
@@ -40,4 +42,4 @@ function colorClick(event) {
   crayon.currentColor = window.getComputedStyle(event.target).backgroundColor;
   var box = document.getElementById("colorBox");
   box.style.backgroundColor = window.getComputedStyle(event.target).backgroundColor;
-}
+};
